0e827021c14722638cef31dea1f3d140bba570bb
trulune <HELMIKEDAHH@GMAIL.COM>
2025-12-25 14:32:39 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,18 @@
 // js/firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyDoBtFtFEs6Cd-uU6dNvwXNAuXOd1R1r10",
-  authDomain: "post-58eb6.firebaseapp.com",
-  projectId: "post-58eb6",
-  storageBucket: "post-58eb6.firebasestorage.app",
-  messagingSenderId: "448356917822",
-  appId: "1:448356917822:web:8251dc0ecff7fcd0efe76f"
+    apiKey: "AIzaSyDoBtFtFEs6Cd-uU6dNvwXNAuXOd1R1r10",
+    authDomain: "post-58eb6.firebaseapp.com",
+    projectId: "post-58eb6",
+    storageBucket: "post-58eb6.firebasestorage.app",
+    messagingSenderId: "448356917822",
+    appId: "1:448356917822:web:8251dc0ecff7fcd0efe76f"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Initialize services
+// Initialize Firestore
 const db = firebase.firestore();
-const storage = firebase.storage();
 
 // Make available globally
 window.db = db;
-window.firebaseStorage = storage;
